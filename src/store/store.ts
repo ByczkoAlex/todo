9d56446f3390866e4cfca9a4a7109f86622c49c9
AlexFront <byczko2012@mail.ru>
2023-09-26 16:23:16 +0300

    added toolkit , added slice

---
+++ src/store/store.ts
@@ -0,0 +1,18 @@
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import {useDispatch} from 'react-redux';
+import todoReducer from './reducers/todo/TodoSlice'
+
+const rootReducer = combineReducers({
+    todoReducer
+})
+
+export const setupStore = () => {
+    return configureStore({
+        reducer: rootReducer
+    })
+}
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppStore = ReturnType<typeof setupStore>
+export type AppDispatch = AppStore['dispatch']
+export const useAppDispatch: () => AppDispatch = useDispatch
